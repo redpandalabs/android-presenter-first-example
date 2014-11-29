806fa43ee01c90051a5d7089fa096af666511d1e
Nitish Rathi <nitish@redpanda.co.in>
2014-11-29 22:37:51 +0530

    remove details activity and saved state handling

--- app/src/main/java/com/latebound/humble/ContactLayout.java
+++ app/src/main/java/com/latebound/humble/ContactLayout.java
@@ -25,31 +25,13 @@ public class ContactLayout extends Activity {
     }
 
     public static class ContactListFragment extends ListFragment {
-        int mCurCheckPosition = 0;
-
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
 
-            // Populate list with our static array of titles.
-            setListAdapter(new ArrayAdapter<String>(getActivity(),
-                    android.R.layout.simple_list_item_activated_1, Shakespeare.TITLES));
+            setListAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_activated_1, Shakespeare.TITLES));
 
-            if (savedInstanceState != null) {
-                // Restore last state for checked position.
-                mCurCheckPosition = savedInstanceState.getInt("curChoice", 0);
-            }
-
-            // In dual-pane mode, the list view highlights the selected item.
             getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-            // Make sure our UI is in the correct state.
-            showDetails(mCurCheckPosition);
-        }
-
-        @Override
-        public void onSaveInstanceState(Bundle outState) {
-            super.onSaveInstanceState(outState);
-            outState.putInt("curChoice", mCurCheckPosition);
         }
 
         @Override
@@ -57,27 +39,14 @@ public class ContactLayout extends Activity {
             showDetails(position);
         }
 
-        /**
-         * Helper function to show the details of a selected item, either by
-         * displaying a fragment in-place in the current UI, or starting a
-         * whole new activity in which it is displayed.
-         */
         void showDetails(int index) {
-            mCurCheckPosition = index;
 
-            // We can display everything in-place with fragments, so update
-            // the list to highlight the selected item and show the data.
             getListView().setItemChecked(index, true);
 
-            // Check what fragment is currently shown, replace if needed.
-            DetailsFragment details = (DetailsFragment)
-                    getFragmentManager().findFragmentById(R.id.details);
+            DetailsFragment details = (DetailsFragment) getFragmentManager().findFragmentById(R.id.details);
             if (details == null || details.getShownIndex() != index) {
-                // Make new fragment to show this selection.
                 details = DetailsFragment.newInstance(index);
 
-                // Execute a transaction, replacing any existing fragment
-                // with this one inside the frame.
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 if (index == 0) {
                     ft.replace(R.id.details, details);
@@ -91,14 +60,9 @@ public class ContactLayout extends Activity {
     }
 
     public static class DetailsFragment extends Fragment {
-        /**
-         * Create a new instance of DetailsFragment, initialized to
-         * show the text at 'index'.
-         */
         public static DetailsFragment newInstance(int index) {
             DetailsFragment f = new DetailsFragment();
 
-            // Supply index input as an argument.
             Bundle args = new Bundle();
             args.putInt("index", index);
             f.setArguments(args);
@@ -114,13 +78,6 @@ public class ContactLayout extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             if (container == null) {
-                // We have different layouts, and in one of them this
-                // fragment's containing frame doesn't exist.  The fragment
-                // may still be created from its saved state, but there is
-                // no reason to try to create its view hierarchy because it
-                // won't be displayed.  Note this is not needed -- we could
-                // just run the code below, where we would create and return
-                // the view hierarchy; it would just never be used.
                 return null;
             }
 
@@ -132,29 +89,6 @@ public class ContactLayout extends Activity {
             scroller.addView(text);
             text.setText(Shakespeare.DIALOGUE[getShownIndex()]);
             return scroller;
-        }
-    }
-
-    public static class DetailsActivity extends Activity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            if (getResources().getConfiguration().orientation
-                    == Configuration.ORIENTATION_LANDSCAPE) {
-                // If the screen is now in landscape mode, we can show the
-                // dialog in-line with the list so we don't need this activity.
-                finish();
-                return;
-            }
-
-            if (savedInstanceState == null) {
-                // During initial setup, plug in the details fragment.
-                DetailsFragment details = new DetailsFragment();
-                details.setArguments(getIntent().getExtras());
-                getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
-            }
         }
     }
 }
