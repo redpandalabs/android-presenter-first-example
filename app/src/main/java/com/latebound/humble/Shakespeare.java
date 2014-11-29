720c8f7120ae13b3548b863b9038208aed0adb2a
Nitish Rathi <nitish@redpanda.co.in>
2014-11-29 21:58:40 +0530

    initial commit

---
+++ app/src/main/java/com/latebound/humble/Shakespeare.java
@@ -0,0 +1,9 @@
+package com.latebound.humble;
+
+/**
+ * Created by latebound on 11/29/14.
+ */
+public class Shakespeare {
+    public static final String[] TITLES = {"foo", "bar"};
+    public static final String[] DIALOGUE = {"foot", "bart"};
+}
